a6f5d46309c0c2e4892260d83c5afcd8a183e3ff
Emmanuel615 <emmanuel.pereyra@bghtechpartner.com>
2022-03-22 15:30:34 -0300

    ejercicio 1.1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,27 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Hello = (props) => {
+const Header = (props) => {
   return (
-    <div>
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        {props.part} {props.num}
       </p>
-    </div>
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <>
+      <p>
+        Number of exercises {props.num1 + props.num2 + props.num3}
+      </p>
+    </>
   );
 };
 
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+    <>
+      <Header course={course} />
+
+      <Content part={part1} num={exercises1} />
+      <Content part={part2} num={exercises2} />
+      <Content part={part3} num={exercises3} />
+
+      <Total num1={exercises1} num2={exercises2} num3={exercises3} />
+    </>
   );
 };
 
-ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
